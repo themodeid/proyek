b08c90c5e877f11ba4b247a2dafbb7e29e1f15c7
themodeid <adamwahyukur@gmail.com>
2025-08-11 17:00:37 +0700

    belajar

--- frontend/dom level/dom/jumlah/jumlah.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("btn").addEventListener("click", function () {
-  let angka1 = parseFloat(document.getElementById("angka1").value);
-  let angka2 = parseFloat(document.getElementById("angka2").value);
-  const jumlah = angka1 + angka2;
-
-  let p = document.getElementById("hasil");
-
-  p.innerText = jumlah;
-})
